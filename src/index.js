ed7ade4ccce0f49709f7b2c004e3f8d2e6b4a6b0
Raymond Cox <raymond@raymondjcox.com>
2019-01-14 20:50:23 -0500

    Add auto suggest interface
    I don't like how this is dependent on the click event happening before
    the ajax response

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import { of } from 'rxjs';
+import { of, fromEvent} from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { flatMap, map, filter } from 'rxjs/operators';
+import { flatMap, switchMap, map, filter, startWith, take, merge } from 'rxjs/operators';
 
-let requestStream = of('https://api.github.com/users');
-let responseStream = requestStream.pipe(
+let refreshButton = document.querySelector('.refresh');
+let refreshClick$ = fromEvent(refreshButton, 'click');
+let container = document.querySelector('.content-container');
+
+let request$ = refreshClick$.pipe(
+  startWith('page load'),
+  map(() => {
+    let randomOffset = Math.floor(Math.random() * 500);
+    return `https://api.github.com/users?since=${randomOffset}`;
+  })
+);
+
+let response$ = request$.pipe(
   flatMap((requestUrl) => ajax.getJSON(requestUrl))
 );
 
-responseStream.subscribe((response) => {
-  console.log(response);
+let suggestions$ = response$.pipe(
+  map((users) => users.slice(0, 3)),
+  switchMap((users) => users),
+  merge(refreshClick$.pipe(map(() => null)))
+);
+
+suggestions$.subscribe((suggestion) => {
+  if (suggestion === null) {
+    while(container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  } else {
+    let ele = document.createElement('div');
+    ele.innerHTML = suggestion.login;
+    container.appendChild(ele);
+  }
 });
