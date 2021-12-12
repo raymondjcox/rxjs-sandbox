cb517a80ac091db91db6f9ea0ecde16100a5d683
Raymond Cox <raymond@raymondjcox.com>
2021-12-11 21:48:02 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
-import { of, fromEvent} from 'rxjs';
-import { ajax } from 'rxjs/ajax';
-import { flatMap, switchMap, map, filter, startWith, take, merge } from 'rxjs/operators';
+import { fromEvent } from "rxjs";
 
-let refreshButton = document.querySelector('.refresh');
-let refreshClick$ = fromEvent(refreshButton, 'click');
-let container = document.querySelector('.content-container');
+const button = document.getElementById("refresh");
 
-let request$ = refreshClick$.pipe(
-  startWith('page load'),
-  map(() => {
-    let randomOffset = Math.floor(Math.random() * 500);
-    return `https://api.github.com/users?since=${randomOffset}`;
-  })
-);
+const myObservable = fromEvent(button, "click");
 
-let response$ = request$.pipe(
-  flatMap((requestUrl) => ajax.getJSON(requestUrl))
-);
-
-let suggestions$ = response$.pipe(
-  map((users) => users.slice(0, 3)),
-  switchMap((users) => users),
-  merge(refreshClick$.pipe(map(() => null)))
-);
-
-suggestions$.subscribe((suggestion) => {
-  if (suggestion === null) {
-    while(container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-  } else {
-    let ele = document.createElement('div');
-    ele.innerHTML = suggestion.login;
-    container.appendChild(ele);
-  }
-});
+const subscription = myObservable.subscribe(event => console.log(event));
+const subscription1 = myObservable.subscribe(event => console.log(event));
